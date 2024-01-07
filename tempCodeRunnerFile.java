e69c420e2066ee2c93f3bfd4ef1f40d2fc6f643e
Dhaval <makanidhaval9619@gmail.com>
2024-01-08 00:01:29 +0530

    JAVA Patterns

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,6 @@
-side
+for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
